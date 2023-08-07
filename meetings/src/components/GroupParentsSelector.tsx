a5764951229aa42b3d6c6a42a8b7c505981d1c53
Robert Stacey <rjstacey@gmail.com>
2023-08-07 15:09:37 -0700

    Add group parent selector

---
+++ meetings/src/components/GroupParentsSelector.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Select } from 'dot11-components';
+
+import { useAppSelector } from '../store/hooks';
+import { selectGroupParents } from '../store/groups';
+
+export function GroupParentsSelector({
+	value,
+	onChange,
+	...otherProps
+}: {
+	value: string | null;
+	onChange: (value: string | null) => void;
+} & Omit<React.ComponentProps<typeof Select>, "values" | "onChange" | "options">)
+{
+	const groups = useAppSelector(selectGroupParents);
+
+	const handleChange = (values: typeof groups) => onChange(values.length > 0? values[0].id: null);
+
+	const values = groups.filter(group => group.id === value);
+
+	return (
+		<Select
+			values={values}
+			onChange={handleChange}
+			options={groups}
+			valueField='id'
+			labelField='name'
+			{...otherProps}
+		/>
+	)
+}
+
+export default GroupParentsSelector;
