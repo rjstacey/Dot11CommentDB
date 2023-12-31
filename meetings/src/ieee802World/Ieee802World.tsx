f8fcd75bbadf0acaa191c203a162210d0f3eb4ea
Robert Stacey <rjstacey@gmail.com>
2023-12-30 20:38:41 -0800

    remove styles

--- meetings/src/ieee802World/Ieee802World.tsx
+++ meetings/src/ieee802World/Ieee802World.tsx
@@ -1,7 +1,5 @@
-import styled from '@emotion/styled';
-
 import {
-	AppTable, 
+	AppTable,
 	SelectHeaderCell,
 	SelectCell,
 	TableColumnSelector,
@@ -12,10 +10,10 @@ import {
 	TableConfig,
 	TablesConfig,
 	HeaderCellRendererProps,
-	RowGetterProps
-} from 'dot11-components';
+	RowGetterProps,
+} from "dot11-components";
 
-import { useAppDispatch } from '../store/hooks';
+import { useAppDispatch } from "../store/hooks";
 import {
 	load802WorldSchedule,
 	importSelectedAsMeetings,
@@ -23,80 +21,123 @@ import {
 	getField,
 	ieee802WorldSelectors,
 	ieee802WorldActions,
-} from '../store/ieee802World';
+} from "../store/ieee802World";
 
-import TopRow from '../components/TopRow';
-import MeetingSummary from '../components/MeetingSummary';
+import TopRow from "../components/TopRow";
+import MeetingSummary from "../components/MeetingSummary";
 
-const TableRow = styled.div`
-	flex: 1;	/* remaining height */
-	width: 100%;
-	align-items: center;
-	.AppTable__dataRow,
-	.AppTable__headerRow {
-		align-items: center;
-	}
-`;
+import styles from "./Ieee802World.module.css";
 
-const renderDateHeader = (props: HeaderCellRendererProps) =>
+const renderDateHeader = (props: HeaderCellRendererProps) => (
 	<>
-		<TableColumnHeader {...props} dataKey='day' label='Day' />
-		<TableColumnHeader {...props} dataKey='date' label='Date' />
+		<TableColumnHeader {...props} dataKey="day" label="Day" />
+		<TableColumnHeader {...props} dataKey="date" label="Date" />
 	</>
+);
 
-const renderTimeRangeHeader = (props: HeaderCellRendererProps) =>
+const renderTimeRangeHeader = (props: HeaderCellRendererProps) => (
 	<>
-		<TableColumnHeader {...props} dataKey='startTime' label='Start time' />
-		<TableColumnHeader {...props} dataKey='endTime' label='End time' />
+		<TableColumnHeader {...props} dataKey="startTime" label="Start time" />
+		<TableColumnHeader {...props} dataKey="endTime" label="End time" />
 	</>
+);
 
 const tableColumns: ColumnProperties[] = [
-	{key: '__ctrl__',
-		width: 30, flexGrow: 0, flexShrink: 0,
-		headerRenderer: p => <SelectHeaderCell {...p} />,
-		cellRenderer: p =>
+	{
+		key: "__ctrl__",
+		width: 30,
+		flexGrow: 0,
+		flexShrink: 0,
+		headerRenderer: (p) => <SelectHeaderCell {...p} />,
+		cellRenderer: (p) => (
 			<SelectCell
 				selectors={ieee802WorldSelectors}
 				actions={ieee802WorldActions}
 				{...p}
-			/>},
-	{key: 'day',
-		...fields.day,
-		width: 60, flexGrow: 1, flexShrink: 0},
-	{key: 'dayDate',
+			/>
+		),
+	},
+	{ key: "day", ...fields.day, width: 60, flexGrow: 1, flexShrink: 0 },
+	{
+		key: "dayDate",
 		...fields.dayDate,
-		width: 100, flexGrow: 1, flexShrink: 0,
-		headerRenderer: renderDateHeader},
-	{key: 'timeRange',
+		width: 100,
+		flexGrow: 1,
+		flexShrink: 0,
+		headerRenderer: renderDateHeader,
+	},
+	{
+		key: "timeRange",
 		...fields.timeRange,
-		width: 70, flexGrow: 1, flexShrink: 0,
-		headerRenderer: renderTimeRangeHeader},
-	{key: 'startTime', 
+		width: 70,
+		flexGrow: 1,
+		flexShrink: 0,
+		headerRenderer: renderTimeRangeHeader,
+	},
+	{
+		key: "startTime",
 		...fields.startTime,
-		width: 100, flexGrow: 1, flexShrink: 1},
-	{key: 'endTime', 
+		width: 100,
+		flexGrow: 1,
+		flexShrink: 1,
+	},
+	{
+		key: "endTime",
 		...fields.endTime,
-		width: 100, flexGrow: 1, flexShrink: 1, dropdownWidth: 300},
-	{key: 'groupName', 
+		width: 100,
+		flexGrow: 1,
+		flexShrink: 1,
+		dropdownWidth: 300,
+	},
+	{
+		key: "groupName",
 		...fields.groupName,
-		width: 100, flexGrow: 1, flexShrink: 1},
-	{key: 'meeting', 
+		width: 100,
+		flexGrow: 1,
+		flexShrink: 1,
+	},
+	{
+		key: "meeting",
 		...fields.meeting,
-		width: 400, flexGrow: 1, flexShrink: 1},
-	{key: 'mtgRoom', 
+		width: 400,
+		flexGrow: 1,
+		flexShrink: 1,
+	},
+	{
+		key: "mtgRoom",
 		...fields.mtgRoom,
-		width: 200, flexGrow: 1, flexShrink: 1},
-	{key: 'mtgLocation', 
+		width: 200,
+		flexGrow: 1,
+		flexShrink: 1,
+	},
+	{
+		key: "mtgLocation",
 		...fields.mtgLocation,
-		width: 300, flexGrow: 1, flexShrink: 1},
-	{key: 'meetingId',
-		label: 'Compare meeting',
-		width: 200, flexGrow: 1, flexShrink: 1,
-		cellRenderer: ({rowData}) => <MeetingSummary meetingId={rowData.meetingId} />}
+		width: 300,
+		flexGrow: 1,
+		flexShrink: 1,
+	},
+	{
+		key: "meetingId",
+		label: "Compare meeting",
+		width: 200,
+		flexGrow: 1,
+		flexShrink: 1,
+		cellRenderer: ({ rowData }) => (
+			<MeetingSummary meetingId={rowData.meetingId} />
+		),
+	},
 ];
 
 const defaultTablesColumns = {
-	default: ['__ctrl__', 'dayDate', 'timeRange', 'groupName', 'meeting', 'mtgRoom'],
+	default: [
+		"__ctrl__",
+		"dayDate",
+		"timeRange",
+		"groupName",
+		"meeting",
+		"mtgRoom",
+	],
 };
 
 let defaultTablesConfig: TablesConfig = {};
@@ -104,15 +145,15 @@ let tableView: keyof typeof defaultTablesColumns;
 for (tableView in defaultTablesColumns) {
 	const tableConfig: TableConfig = {
 		fixed: false,
-		columns: {}
-	}
+		columns: {},
+	};
 	for (const column of tableColumns) {
 		const key = column.key;
 		tableConfig.columns[key] = {
-			unselectable: key.startsWith('__'),
+			unselectable: key.startsWith("__"),
 			shown: defaultTablesColumns[tableView].includes(key),
-			width: column.width || 200
-		}
+			width: column.width || 200,
+		};
 	}
 	defaultTablesConfig[tableView] = tableConfig;
 }
@@ -120,19 +161,19 @@ for (tableView in defaultTablesColumns) {
 /*
  * Don't display date and time if it is the same as previous line
  */
-function schedRowGetter({rowIndex, ids, entities}: RowGetterProps) {
+function schedRowGetter({ rowIndex, ids, entities }: RowGetterProps) {
 	let b = entities[ids[rowIndex]];
 	b = {
 		...b,
-		dayDate: getField(b, 'dayDate'),
-		timeRange: getField(b, 'timeRange')
+		dayDate: getField(b, "dayDate"),
+		timeRange: getField(b, "timeRange"),
 	};
 	if (rowIndex > 0) {
 		let b_prev = entities[ids[rowIndex - 1]];
-		if (b.dayDate === getField(b_prev, 'dayDate')) {
-			b = {...b, dayDate: ''};
-			if (b.timeRange === getField(b_prev, 'timeRange'))
-				b = {...b, timeRange: ''};
+		if (b.dayDate === getField(b_prev, "dayDate")) {
+			b = { ...b, dayDate: "" };
+			if (b.timeRange === getField(b_prev, "timeRange"))
+				b = { ...b, timeRange: "" };
 		}
 	}
 	return b;
@@ -144,24 +185,31 @@ function Ieee802WorldSchedule() {
 	const refresh = () => dispatch(load802WorldSchedule());
 
 	const importSelected = async () => {
-		const ok = await ConfirmModal.show('Import selected?');
-		if (ok)
-			dispatch(importSelectedAsMeetings());
-	}
+		const ok = await ConfirmModal.show("Import selected?");
+		if (ok) dispatch(importSelectedAsMeetings());
+	};
 
 	return (
 		<>
-			<TopRow style={{justifyContent: 'flex-end'}}>
+			<TopRow style={{ justifyContent: "flex-end" }}>
 				<TableColumnSelector
 					selectors={ieee802WorldSelectors}
 					actions={ieee802WorldActions}
 					columns={tableColumns}
 				/>
-				<ActionButton name='import' title='Import selected' onClick={importSelected} />
-				<ActionButton name='refresh' title='Refresh' onClick={refresh} />
+				<ActionButton
+					name="import"
+					title="Import selected"
+					onClick={importSelected}
+				/>
+				<ActionButton
+					name="refresh"
+					title="Refresh"
+					onClick={refresh}
+				/>
 			</TopRow>
 
-			<TableRow>
+			<div className={styles["table-conainer"]}>
 				<AppTable
 					defaultTablesConfig={defaultTablesConfig}
 					columns={tableColumns}
@@ -171,9 +219,9 @@ function Ieee802WorldSchedule() {
 					actions={ieee802WorldActions}
 					rowGetter={schedRowGetter}
 				/>
-			</TableRow>
+			</div>
 		</>
-	)
+	);
 }
 
 export default Ieee802WorldSchedule;
