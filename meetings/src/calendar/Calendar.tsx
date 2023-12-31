948dd5c7834f841ab1ad0ccb528fae98137b8af6
Robert Stacey <rjstacey@gmail.com>
2023-12-30 17:30:46 -0800

    replace styled

--- meetings/src/calendar/Calendar.tsx
+++ meetings/src/calendar/Calendar.tsx
@@ -1,50 +1,48 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
 
-import { useAppSelector } from '../store/hooks';
-import { selectWorkingGroupId } from '../store/groups';
+import { useAppSelector } from "../store/hooks";
+import { selectWorkingGroupId } from "../store/groups";
 import {
 	selectCalendarAccountsState,
-	CalendarAccount
-} from '../store/calendarAccounts';
+	CalendarAccount,
+} from "../store/calendarAccounts";
 
-const MainIframe = styled.iframe`
-	flex: 1;
-	width: 100%;
-	border: 0;
-`;
+import styles from "./Calendar.module.css";
 
 function Calendar() {
-	const {entities} = useAppSelector(selectCalendarAccountsState);
+	const { entities } = useAppSelector(selectCalendarAccountsState);
 	const groupId = useAppSelector(selectWorkingGroupId);
 
 	const calendarLink = React.useMemo(() => {
-		if (!groupId)
-			return undefined;
-		for (const account of (Object.values(entities) as CalendarAccount[])) {
+		if (!groupId) return undefined;
+		for (const account of Object.values(entities) as CalendarAccount[]) {
 			if (account.groupId === groupId && account.details) {
-				const {details} = account;
+				const { details } = account;
 				// see https://support.google.com/calendar/thread/23205641/advanced-embed-option-descriptions?hl=en
 				const params = {
 					src: details.id,
-					mode: 'WEEK',	// WEEK, AGENDA or none for MONTH
-					ctz: 'America/New_York',
-					wkst: '1',		// 1 = Sunday, 2 = Monday, etc.
-					showPrint: '0',
-					showTitle: '0',
-					showTz: '0',
-				}	
-				return `https://calendar.google.com/calendar/embed?` + new URLSearchParams(params);
+					mode: "WEEK", // WEEK, AGENDA or none for MONTH
+					ctz: "America/New_York",
+					wkst: "1", // 1 = Sunday, 2 = Monday, etc.
+					showPrint: "0",
+					showTitle: "0",
+					showTz: "0",
+				};
+				return (
+					`https://calendar.google.com/calendar/embed?` +
+					new URLSearchParams(params)
+				);
 			}
 		}
 	}, [entities, groupId]);
 
 	return (
-		<MainIframe 
-			title='Google calendar'
+		<iframe
+			className={styles.main}
+			title="Google calendar"
 			src={calendarLink}
 		/>
-	)
+	);
 }
 
 export default Calendar;
