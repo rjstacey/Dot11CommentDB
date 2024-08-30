8a004827e6e432b966147752c206ef536cfa8146
Robert Stacey <rjstacey@gmail.com>
2024-08-30 08:42:35 -0700

    routes

---
+++ meetings/src/components/ImatBreakoutInfo.tsx
@@ -0,0 +1,42 @@
+import { displayDayDate, displayTime } from "dot11-components";
+import { useAppSelector } from "../store/hooks";
+import {
+	selectBreakoutEntities,
+	selectBreakoutMeetingId,
+} from "../store/imatBreakouts";
+
+function ImatBreakoutInfo({
+	imatMeetingId,
+	breakoutId,
+}: {
+	imatMeetingId: number;
+	breakoutId: number;
+}) {
+	const imatBreakoutEntities = useAppSelector(selectBreakoutEntities);
+	const breakoutMeetingId = useAppSelector(selectBreakoutMeetingId);
+	let breakout =
+		breakoutMeetingId === imatMeetingId
+			? imatBreakoutEntities[breakoutId]
+			: undefined;
+
+	let content = breakout ? (
+		<>
+			<span>{breakout.name}</span>
+			<span>{displayDayDate(breakout.start)}</span>
+			<span>
+				{displayTime(breakout.start) +
+					" - " +
+					displayTime(breakout.end)}
+			</span>
+			<span>{breakout.location}</span>
+		</>
+	) : null;
+
+	return (
+		<div style={{ display: "flex", flexDirection: "column" }}>
+			{content}
+		</div>
+	);
+}
+
+export default ImatBreakoutInfo;
---
+++ meetings/src/components/ImatMeetingInfo.tsx
@@ -0,0 +1,22 @@
+import { displayDateRange } from "dot11-components";
+import { useAppSelector } from "../store/hooks";
+import { selectImatMeetingEntities } from "../store/imatMeetings";
+
+function ImatMeetingInfo({ imatMeetingId }: { imatMeetingId: number }) {
+	const imatMeetingEntities = useAppSelector(selectImatMeetingEntities);
+	const imatMeeting = imatMeetingEntities[imatMeetingId];
+	let content = imatMeeting ? (
+		<>
+			<span>{imatMeeting.name}</span>
+			<span>{displayDateRange(imatMeeting.start, imatMeeting.end)}</span>
+			<span>{imatMeeting.timezone}</span>
+		</>
+	) : null;
+	return (
+		<div style={{ display: "flex", flexDirection: "column" }}>
+			{content}
+		</div>
+	);
+}
+
+export default ImatMeetingInfo;
