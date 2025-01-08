14640667b2e1e67e29a2709dfc10794e8cbbffd8
Robert Stacey <rjstacey@gmail.com>
2025-01-08 10:07:03 -0800

    dependencies

--- home/src/app/index.tsx
+++ home/src/app/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./routes";
 
-const getRouter = () => createBrowserRouter(routes, { basename: "/" });
+const getRouter = () => createBrowserRouter(routes, { basename: "/home" });
 
 function App() {
 	// Create routes on initial render. If done at init time, createBrowserRouter() will kick off the loaders before fetcher has been initialized.
