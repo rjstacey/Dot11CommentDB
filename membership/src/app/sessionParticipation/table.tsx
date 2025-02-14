49b7f7e3274be21fd43a66b7ceb557b8acfa9402
Robert Stacey <robert.stacey@intel.com>
2025-02-14 14:21:49 -0800

    columns

--- membership/src/app/sessionParticipation/table.tsx
+++ membership/src/app/sessionParticipation/table.tsx
@@ -1,14 +1,5 @@
-import React from "react";
-import { DateTime } from "luxon";
-
 import {
 	AppTable,
-	SelectHeaderCell,
-	SelectCell,
-	TableColumnHeader,
-	HeaderCellRendererProps,
-	ColumnProperties,
-	CellRendererProps,
 	ShowFilters,
 	GlobalFilter,
 	SplitPanel,
@@ -24,148 +15,35 @@ import {
 	sessionParticipationActions,
 	selectSessionParticipationSelected,
 	setSessionParticipationSelected,
-	type MemberAttendances,
 } from "@/store/sessionParticipation";
-import { selectRecentSessions } from "@/store/sessions";
-import { SessionAttendanceSummary } from "@/store/attendanceSummary";
 
-import { renderNameAndEmail } from "../members/tableColumns";
 import { MemberDetail } from "../members/detail";
+import { useTableColumns, useDefaultTablesConfig } from "./tableColumns";
 
-const renderHeaderNameAndEmail = (props: HeaderCellRendererProps) => (
-	<>
-		<TableColumnHeader {...props} dataKey="Name" label="Name" />
-		<TableColumnHeader {...props} dataKey="Email" label="Email" />
-	</>
-);
-
-const renderSessionAttendance = (
-	notRelevant: boolean,
-	attendance: SessionAttendanceSummary
-) => (
-	<div
-		style={{
-			display: "flex",
-			flexDirection: "column",
-			alignItems: "flex-end",
-			color: notRelevant ? "gray" : "unset",
-		}}
-	>
-		<span>{(attendance.AttendancePercentage || 0).toFixed(1) + "%"}</span>
-		<span>
-			{attendance.DidAttend
-				? "Did attend"
-				: attendance.DidNotAttend
-					? "Did not attend"
-					: ""}
-		</span>
-	</div>
-);
-
-const tableColumns: ColumnProperties[] = [
-	{
-		key: "__ctrl__",
-		width: 30,
-		flexGrow: 1,
-		flexShrink: 0,
-		headerRenderer: SelectHeaderCell,
-		cellRenderer: (p) => (
-			<SelectCell
+export function SessionParticipationTableActions() {
+	const tableColumns = useTableColumns();
+	return (
+		<>
+			<TableColumnSelector
 				selectors={sessionParticipationSelectors}
 				actions={sessionParticipationActions}
-				{...p}
+				columns={tableColumns}
 			/>
-		),
-	},
-	{ key: "SAPIN", label: "SA PIN", width: 80, flexGrow: 1, flexShrink: 1 },
-	{
-		key: "Name",
-		label: "Name",
-		width: 200,
-		flexGrow: 1,
-		flexShrink: 1,
-		headerRenderer: renderHeaderNameAndEmail,
-		cellRenderer: renderNameAndEmail,
-	},
-	{
-		key: "Affiliation",
-		label: "Affiliation",
-		width: 300,
-		flexGrow: 1,
-		flexShrink: 1,
-	},
-	{ key: "Status", label: "Status", width: 150, flexGrow: 1, flexShrink: 1 },
-	{
-		key: "ExpectedStatus",
-		label: "Expected status",
-		width: 150,
-		flexGrow: 1,
-		flexShrink: 1,
-	},
-	{
-		key: "Summary",
-		label: "Summary",
-		width: 100,
-		flexGrow: 1,
-		flexShrink: 1,
-	},
-];
-
-export const SessionParticipationTableActions = () => (
-	<>
-		<TableColumnSelector
-			selectors={sessionParticipationSelectors}
-			actions={sessionParticipationActions}
-			columns={tableColumns}
-		/>
-		<SplitPanelButton
-			selectors={sessionParticipationSelectors}
-			actions={sessionParticipationActions}
-		/>
-	</>
-);
+			<SplitPanelButton
+				selectors={sessionParticipationSelectors}
+				actions={sessionParticipationActions}
+			/>
+		</>
+	);
+}
 
 export function SessionParticipationTable() {
 	const dispatch = useAppDispatch();
 
 	const selected = useAppSelector(selectSessionParticipationSelected);
-	const sessions = useAppSelector(selectRecentSessions);
 
-	const columns = React.useMemo(() => {
-		return tableColumns.concat(
-			sessions.map((session, i) => {
-				const cellRenderer = ({
-					rowData,
-				}: CellRendererProps<MemberAttendances>) => {
-					const attendance = rowData.sessionAttendanceSummaries.find(
-						(a) => a.session_id === session.id
-					);
-					const notRelevant =
-						!!rowData.NonVoterDate &&
-						DateTime.fromISO(session.startDate) <
-							DateTime.fromISO(rowData.NonVoterDate);
-					return attendance
-						? renderSessionAttendance(notRelevant, attendance)
-						: null;
-				};
-				const yearMonth = DateTime.fromISO(session.startDate).toFormat(
-					"yyyy MMM"
-				);
-				const column = {
-					key: "session_" + i,
-					label:
-						(session.type || "?").toLocaleUpperCase() +
-						": " +
-						yearMonth,
-					width: 100,
-					flexGrow: 1,
-					flexShrink: 1,
-					cellRenderer,
-				};
-				return column;
-			})
-		);
-	}, [sessions]);
+	const columns = useTableColumns();
+	const defaultTablesConfig = useDefaultTablesConfig();
 
 	return (
 		<>
@@ -194,6 +72,7 @@ export function SessionParticipationTable() {
 						estimatedRowHeight={50}
 						selectors={sessionParticipationSelectors}
 						actions={sessionParticipationActions}
+						defaultTablesConfig={defaultTablesConfig}
 					/>
 				</Panel>
 				<Panel className="details-panel">
---
+++ membership/src/app/sessionParticipation/tableColumns.tsx
@@ -0,0 +1,161 @@
+import * as React from "react";
+import { DateTime } from "luxon";
+import {
+	SelectHeaderCell,
+	SelectCell,
+	ColumnProperties,
+	CellRendererProps,
+	TablesConfig,
+	TableConfig,
+} from "dot11-components";
+import { useAppSelector } from "@/store/hooks";
+import { selectRecentSessions } from "@/store/sessions";
+import {
+	sessionParticipationSelectors,
+	sessionParticipationActions,
+	type MemberAttendances,
+} from "@/store/sessionParticipation";
+import type { SessionAttendanceSummary } from "@/store/attendanceSummary";
+import {
+	renderNameAndEmail,
+	renderHeaderNameAndEmail,
+} from "../members/tableColumns";
+
+const renderSessionAttendance = (
+	notRelevant: boolean,
+	attendance: SessionAttendanceSummary
+) => (
+	<div
+		style={{
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "flex-end",
+			color: notRelevant ? "gray" : "unset",
+		}}
+	>
+		<span>{(attendance.AttendancePercentage || 0).toFixed(1) + "%"}</span>
+		<span>
+			{attendance.DidAttend
+				? "Did attend"
+				: attendance.DidNotAttend
+					? "Did not attend"
+					: ""}
+		</span>
+	</div>
+);
+
+const tableColumns: ColumnProperties[] = [
+	{
+		key: "__ctrl__",
+		width: 30,
+		flexGrow: 1,
+		flexShrink: 0,
+		headerRenderer: SelectHeaderCell,
+		cellRenderer: (p) => (
+			<SelectCell
+				selectors={sessionParticipationSelectors}
+				actions={sessionParticipationActions}
+				{...p}
+			/>
+		),
+	},
+	{ key: "SAPIN", label: "SA PIN", width: 80, flexGrow: 1, flexShrink: 1 },
+	{
+		key: "Name",
+		label: "Name",
+		width: 200,
+		flexGrow: 1,
+		flexShrink: 1,
+		headerRenderer: renderHeaderNameAndEmail,
+		cellRenderer: renderNameAndEmail,
+	},
+	{
+		key: "Affiliation",
+		label: "Affiliation",
+		width: 300,
+		flexGrow: 1,
+		flexShrink: 1,
+	},
+	{ key: "Status", label: "Status", width: 150, flexGrow: 1, flexShrink: 1 },
+	{
+		key: "ExpectedStatus",
+		label: "Expected status",
+		width: 150,
+		flexGrow: 1,
+		flexShrink: 1,
+	},
+	{
+		key: "Summary",
+		label: "Summary",
+		width: 100,
+		flexGrow: 1,
+		flexShrink: 1,
+	},
+];
+
+export function useTableColumns() {
+	const sessions = useAppSelector(selectRecentSessions);
+
+	const columns = React.useMemo(() => {
+		return tableColumns.concat(
+			sessions.map((session, i) => {
+				const cellRenderer = ({
+					rowData,
+				}: CellRendererProps<MemberAttendances>) => {
+					const attendance = rowData.sessionAttendanceSummaries.find(
+						(a) => a.session_id === session.id
+					);
+					const notRelevant =
+						!!rowData.NonVoterDate &&
+						DateTime.fromISO(session.startDate) <
+							DateTime.fromISO(rowData.NonVoterDate);
+					return attendance
+						? renderSessionAttendance(notRelevant, attendance)
+						: null;
+				};
+				const yearMonth = DateTime.fromISO(session.startDate).toFormat(
+					"yyyy MMM"
+				);
+				const column = {
+					key: "session_" + i,
+					label:
+						(session.type || "?").toLocaleUpperCase() +
+						": " +
+						yearMonth,
+					width: 100,
+					flexGrow: 1,
+					flexShrink: 1,
+					cellRenderer,
+				};
+				return column;
+			})
+		);
+	}, [sessions]);
+
+	return columns;
+}
+
+export function useDefaultTablesConfig() {
+	const columns = useTableColumns();
+
+	const defaultTablesConfig = React.useMemo(() => {
+		const defaultTablesConfig: TablesConfig = {};
+		const tableView = "default";
+		const tableConfig: TableConfig = {
+			fixed: false,
+			columns: {},
+		};
+		for (const column of columns) {
+			const key = column.key;
+			tableConfig.columns[key] = {
+				unselectable: key.startsWith("__"),
+				shown: true,
+				width: column.width || 200,
+			};
+		}
+		defaultTablesConfig[tableView] = tableConfig;
+		return defaultTablesConfig;
+	}, [columns]);
+
+	return defaultTablesConfig;
+}
