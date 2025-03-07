3170818f971003eef105c8bcebc1754836c7134e
Robert Stacey <robert.stacey@intel.com>
2025-03-07 14:10:21 -0800

    router

--- membership/src/app/sessionAttendance/layout.tsx
+++ membership/src/app/sessionAttendance/layout.tsx
@@ -1,28 +1,11 @@
-import { Routes, Route } from "react-router";
+import { Outlet } from "react-router";
 import { SessionAttendanceActions } from "./actions";
-import { SessionAttendanceTable } from "./table";
-import { SessionAttendanceChart } from "./chart";
-import { SessionRegistrationTable } from "./registration";
-import { sessionAttendanceChartLoader } from "./loader";
 
 export function SessionAttendance() {
 	return (
 		<>
 			<SessionAttendanceActions />
-			<Routes>
-				<Route path=":sessionNumber">
-					<Route index element={<SessionAttendanceTable />} />
-					<Route
-						path="chart"
-						loader={sessionAttendanceChartLoader}
-						element={<SessionAttendanceChart />}
-					/>
-					<Route
-						path="registration"
-						element={<SessionRegistrationTable />}
-					/>
-				</Route>
-			</Routes>
+			<Outlet />
 		</>
 	);
 }
