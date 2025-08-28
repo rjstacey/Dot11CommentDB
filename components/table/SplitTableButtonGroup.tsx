430e3c8c9e0590f480ffb0b7b19b95bf033903cb
Robert Stacey <robert.stacey@intel.com>
2025-08-28 07:38:33 -0700

    fix: restructure

--- components/table/SplitTableButtonGroup.tsx
+++ components/table/SplitTableButtonGroup.tsx
@@ -1,29 +1,26 @@
+import React from "react";
+import { Col } from "react-bootstrap";
 import TableViewSelector from "./TableViewSelector";
 import TableColumnSelector from "./TableColumnSelector";
 import { SplitPanelButton } from "./SplitPanel";
-
 import type { ColumnProperties } from "./AppTable";
 import type {
 	AppTableDataSelectors,
 	AppTableDataActions,
 } from "../store/appTableData";
-import React from "react";
-
-type SplitTableButtonGroupProps = {
-	columns: Array<ColumnProperties>;
-	selectors: AppTableDataSelectors;
-	actions: AppTableDataActions;
-	style?: React.CSSProperties;
-};
 
 export function SplitTableButtonGroup({
 	columns,
 	selectors,
 	actions,
-	style,
-}: SplitTableButtonGroupProps) {
+	...props
+}: {
+	columns: ColumnProperties[];
+	selectors: AppTableDataSelectors;
+	actions: AppTableDataActions;
+} & React.ComponentProps<typeof Col>) {
 	return (
-		<div className="d-flex align-items-center gap-2" style={style}>
+		<Col className="d-flex align-items-center gap-2" {...props}>
 			<div>Table view</div>
 			<TableViewSelector selectors={selectors} actions={actions} />
 			<TableColumnSelector
@@ -32,7 +29,7 @@ export function SplitTableButtonGroup({
 				actions={actions}
 			/>
 			<SplitPanelButton selectors={selectors} actions={actions} />
-		</div>
+		</Col>
 	);
 }
 
