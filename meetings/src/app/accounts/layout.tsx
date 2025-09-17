08b3f0e3b17d566495d848a8d264cba0142397a5
Robert Stacey <robert.stacey@intel.com>
2025-09-17 20:30:26 +0200

    fix: layout

--- meetings/src/app/accounts/layout.tsx
+++ meetings/src/app/accounts/layout.tsx
@@ -1,13 +1,12 @@
 import CalendarAccounts from "./CalendarAccounts";
 import WebexAccounts from "./WebexAccounts";
-import styles from "./accounts.module.css";
 
 function AccountsLayout() {
 	return (
-		<div className={styles.container}>
+		<>
 			<WebexAccounts />
 			<CalendarAccounts />
-		</div>
+		</>
 	);
 }
 
