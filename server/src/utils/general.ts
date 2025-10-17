4609ad389c87f67550af729c56cc89ca2e8a16c2
Robert Stacey <robert.stacey@intel.com>
2025-10-17 12:23:16 -0700

    fix: parseNumber not used

--- server/src/utils/general.ts
+++ server/src/utils/general.ts
@@ -1,17 +1,4 @@
 // eslint-disable-next-line
-export const parseNumber = (value: any) => {
-	// Return the value as-is if it's already a number
-	if (typeof value === "number") return value;
-
-	// Build regex to strip out everything except digits, decimal point and minus sign
-	const regex = new RegExp("[^0-9-.]", "g");
-	const unformatted = parseFloat(("" + value).replace(regex, ""));
-
-	// This will fail silently
-	return !isNaN(unformatted) ? unformatted : 0;
-};
-
-// eslint-disable-next-line
 export const isPlainObject = (obj: unknown): obj is Record<string, any> =>
 	obj !== null &&
 	typeof obj == "object" &&
