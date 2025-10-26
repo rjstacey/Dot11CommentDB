ff0a8417d6ee706b2086e5e9c7d09a13e37b8387
Robert Stacey <robert.stacey@intel.com>
2025-10-26 11:23:12 -0700

    fix: common account dropdown

--- polling/src/app/header/AccountDropdown.tsx
+++ polling/src/app/header/AccountDropdown.tsx
@@ -1,26 +1,23 @@
 import { Dropdown } from "react-bootstrap";
-
 import { loginAndReturn } from "@common";
-import { resetStore, selectUser, setUser } from "@/store";
+import { resetStore, persistor, selectUser, setUser } from "@/store";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 
-import pkg from "../../../package.json";
+import pkg from "../../../package.json" with { type: "json" };
 
-export function AccountDropdown({ className }: { className?: string }) {
+export function AccountDropdown() {
 	const dispatch = useAppDispatch();
 	const user = useAppSelector(selectUser);
-	const reload = () => {
+	const reload = async () => {
 		dispatch(resetStore());
 		dispatch(setUser(user));
+		await persistor.flush();
 		window.location.reload();
 	};
 
 	return (
-		<Dropdown id="account-dropdown" className={className}>
-			<Dropdown.Toggle
-				variant="outline-secondary"
-				id="account-dropdown-toggle"
-			>
+		<Dropdown id="account-dropdown">
+			<Dropdown.Toggle variant="outline-secondary" id="account-dropdown-toggle">
 				{`${user.Name} (${user.SAPIN})`}
 			</Dropdown.Toggle>
 			<Dropdown.Menu align="end">
