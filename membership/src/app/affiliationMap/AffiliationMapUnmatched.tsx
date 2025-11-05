6fd7cb205c0f722779a10fcae5606bf178d0f85a
Robert Stacey <robert.stacey@intel.com>
2025-11-05 14:53:11 -0800

    centralize matchRegExp

--- membership/src/app/affiliationMap/AffiliationMapUnmatched.tsx
+++ membership/src/app/affiliationMap/AffiliationMapUnmatched.tsx
@@ -1,20 +1,7 @@
 import * as React from "react";
-import { AffiliationMap, selectAffiliationMaps } from "@/store/affiliationMap";
+import { selectAffiliationMaps, matchRegExp } from "@/store/affiliationMap";
 import { useAppSelector } from "@/store/hooks";
 import { selectActiveMembers } from "@/store/members";
-
-function matchRegExp(map: AffiliationMap) {
-	const parts = map.match.split("/");
-	let re: RegExp;
-	try {
-		if ((parts.length === 2 || parts.length === 3) && parts[0] === "")
-			re = new RegExp(parts[1], parts[2]);
-		else re = new RegExp(map.match);
-	} catch {
-		return;
-	}
-	return re;
-}
 
 function AffiliationMapUnmatched() {
 	const maps = useAppSelector(selectAffiliationMaps);
@@ -22,7 +9,7 @@ function AffiliationMapUnmatched() {
 	const unmatchedMembers = React.useMemo(() => {
 		let unmatched = members.map((m) => m.Affiliation);
 		for (const map of maps) {
-			const re = matchRegExp(map);
+			const re = matchRegExp(map.match);
 			if (re) unmatched = unmatched.filter((m) => !re.test(m));
 		}
 
