c37a89c13221e329655bb3188bf0c4bd982eb6ee
Robert Stacey <robert.stacey@intel.com>
2026-04-14 12:53:16 -0700

    vite remote

--- polling/vite.config.ts
+++ polling/vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig, loadEnv, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-
-const target = "http://localhost:8080";
+import { ProxyAgent } from "proxy-agent";
 
 export default defineConfig(({ command, mode }) => {
 	const __dirname = process.cwd();
 	const env = { ...loadEnv(mode, __dirname, "") };
 	if (command === "build" && !env.BUILD_PATH)
 		throw Error("BUILD_PATH not set");
+	let target = "http://localhost:8080";
+	if (mode === "remote") {
+		if (!env.REMOTE_SERVER) throw Error("REMOTE_SERVER not set");
+		target = env.REMOTE_SERVER;
+	}
 	if (!env.BASE_URL) throw Error("BASE_URL not set");
 	return {
 		base: env.BASE_URL,
@@ -48,11 +52,13 @@ export default defineConfig(({ command, mode }) => {
 				"^(/api|/auth|/login|/logout)": {
 					target,
 					changeOrigin: true,
+					agent: new ProxyAgent(),
 				},
 				"/socket.io": {
 					target,
 					changeOrigin: true,
 					ws: true,
+					agent: new ProxyAgent(),
 				},
 			},
 		},
