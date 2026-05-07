58ce03f5d8e12bd1174d3d6402aa47eb56c0f2a9
Robert Stacey <robert.stacey@intel.com>
2026-05-07 11:31:30 -0700

    header

--- meetings/src/app/header/index.tsx
+++ meetings/src/app/header/index.tsx
@@ -1,19 +1,14 @@
-import { Container } from "react-bootstrap";
 import { Menu } from "./Menu";
 import { AccountDropdown } from "./AccountDropdown";
 import { useMenuItems } from "./useMenuItems";
 
-function Header() {
+function Header(props: React.ComponentProps<"header">) {
 	const menuItems = useMenuItems();
 	return (
-		<Container
-			as="header"
-			fluid
-			className="d-flex flex-row justify-content-between align-items-center gap-2 bg-body-tertiary"
-		>
+		<header {...props}>
 			<Menu>{menuItems}</Menu>
 			<AccountDropdown />
-		</Container>
+		</header>
 	);
 }
 
