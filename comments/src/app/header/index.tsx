4446b3dd9e383536ce360010df021e39b8eb90e4
Robert Stacey <robert.stacey@intel.com>
2026-05-07 11:38:00 -0700

    header

--- comments/src/app/header/index.tsx
+++ comments/src/app/header/index.tsx
@@ -1,21 +1,20 @@
-import { Container } from "react-bootstrap";
 import { AccountDropdown } from "./AccountDropdown";
 import { LiveUpdateSwitch } from "./LiveUpdateSwitch";
 import { OnlineIndicator } from "./OnlineIndicator";
 import { Menu } from "./Menu";
 import { useMenuItems } from "./useMenuItems";
 
-function Header() {
+function Header(props: React.ComponentProps<"header">) {
 	const menuItems = useMenuItems();
 	return (
-		<Container as="header" fluid className="bg-body-tertiary">
+		<header {...props}>
 			<Menu>{menuItems}</Menu>
 			<div className="d-flex justify-content-end align-items-center gap-2 p-2">
 				<OnlineIndicator />
 				<LiveUpdateSwitch />
 				<AccountDropdown />
 			</div>
-		</Container>
+		</header>
 	);
 }
 
